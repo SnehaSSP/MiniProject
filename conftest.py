34d9df662a93cabcde9e1693af33ca61c6ed4c87
SnehaSS <138198949+SnehaSSP@users.noreply.github.com>
2025-04-25 15:39:21 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,17 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="session")
 def driver():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')          # Run in headless mode
+    chrome_options.add_argument('--no-sandbox')         # Bypass OS security model
+    chrome_options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     yield driver
     driver.quit()
-
